eb75c3e22be4476cca4695fdb16705c33bb7ee03
fuhuodemao@163.com <fuhuodemao@163.com>
2017-11-09 14:20:29 +0800

    分离出来的前端代码

---
+++ app/store/reducers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by easterCat on 2017/10/16.
+ */
+import article from '../component/article/article.reducer';
+import session from '../component/user/user.reducer';
+export default {
+    article,
+    session
+}
