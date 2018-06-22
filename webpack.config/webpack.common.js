027e566b142e4de62feede8ab85adb9d2f1c50fe
easterCat <fuhuodemao@163.com>
2018-06-22 10:11:29 +0800

    'eslint'

--- webpack.config/webpack.common.js
+++ webpack.config/webpack.common.js
@@ -8,10 +8,10 @@ const webpack = require('webpack');
 const ROOT_PATH = path.resolve(__dirname, '..');
 
 module.exports = {
-    context: path.resolve(ROOT_PATH, 'app'),
+    context: path.resolve(ROOT_PATH, './app'),
     resolve: {
         modules: [
-            path.resolve(ROOT_PATH, 'node_modules'),
+            path.resolve(ROOT_PATH, './node_modules'),
         ],
         alias: {
             'jQuery': 'jquery/dist/jquery',
@@ -26,7 +26,7 @@ module.exports = {
         app: ['babel-polyfill', './index.js'],
     },
     output: {
-        path: path.resolve(ROOT_PATH, 'dist'), //打包的文件夹
+        path: path.resolve(ROOT_PATH, './dist'), //打包的文件夹
         filename: 'index.js',
     },
     module: {
@@ -96,7 +96,7 @@ module.exports = {
         ]
     },
     devServer: {
-        contentBase: path.resolve(ROOT_PATH, 'app'),//为一个目录下的文件提供本地服务器，在这里设置其所在目录
+        contentBase: path.resolve(ROOT_PATH, './app'),//为一个目录下的文件提供本地服务器，在这里设置其所在目录
         historyApiFallback: true,//跳转将指向index.html
         inline: true,//开启自动刷新页面
         port: 1333,//设置监听端口1333
